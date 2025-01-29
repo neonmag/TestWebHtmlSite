a80a3770a5b26efbc299d7328cd9b3281397445c
neonmag <44729614+neonmag@users.noreply.github.com>
2025-01-29 18:02:14 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>CV</h1>
+      <div>
+        <h2>Fullstack ASP.NET\Angular</h2>
+        <h2>Linchevskyi Boryslav</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
